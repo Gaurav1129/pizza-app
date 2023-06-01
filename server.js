8a31ac593486d110d9e1d11a5094c1450afdffa8
Gaurav1129 <121231831+Gaurav1129@users.noreply.github.com>
2023-06-01 16:06:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/orders", require("./routes/orderRoute"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
